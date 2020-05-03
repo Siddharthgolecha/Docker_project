b8f62e4a5693279137dc4e4c8f662a6f44784690
Siddharth Golecha <40235858+Siddharthgolecha@users.noreply.github.com>
2020-05-03 22:55:25 +0530

    These files contains server side scripts.
    This folder contains adding score to database and retrieving scores from the database scripts.

---
+++ php files/display.php
@@ -0,0 +1,24 @@
+<?php
+    // Configuration
+    $hostname = '172.17.0.2';
+    $username = 'siddharth';
+    $password = 'siddharth';
+    $database = 'scores';
+ 
+    try {
+        $dbh = new PDO('mysql:host='. $hostname .';dbname='. $database, $username, $password);
+    } catch(PDOException $e) {
+        echo '<h1>An error has occurred.</h1><pre>', $e->getMessage() ,'</pre>';
+    }
+ 
+    $sth = $dbh->query('SELECT * FROM scores ORDER BY score DESC LIMIT 10');
+    $sth->setFetchMode(PDO::FETCH_ASSOC);
+ 
+    $result = $sth->fetchAll();
+ 
+    if(count($result) > 0) {
+        foreach($result as $r) {
+            echo $r['name'], "\t", $r['score'], "\n";
+        }
+    }
+?>
